4cf8e23d2576f85f6bc3017f07f5e8e065212420
Kristian Setälä <kristian.setala@gmail.com>
2015-04-10 12:00:13 +0300

    validate required fields for author

--- lib/entry_validator.rb
+++ lib/entry_validator.rb
@@ -3,6 +3,10 @@ module EntryValidator
     return false if !entry['entry_type']
     return false if !entry['cite_key']
     return false if !entry['author']
+    return false if !entry['title']
+    return false if !entry['journal']
+    return false if !entry['year']
+    return false if !entry['volume']
     true
   end
 end
