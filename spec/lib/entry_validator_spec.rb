ba92732ebef52132e369788fd308b1c894d3b5c2
Kristian Setälä <kristian.setala@gmail.com>
2015-04-10 11:47:43 +0300

    basic validation

--- spec/lib/entry_validator_spec.rb
+++ spec/lib/entry_validator_spec.rb
@@ -1,6 +1,41 @@
 require 'rails_helper'
+require 'entry_validator'
 
-RSpec.describe EntryValidator do
-  pending "pending"
+RSpec.describe 'EntryValidator' do
+  let(:article_entry) { {
+    'entry_type' => 'article',
+    'cite_key' => 'art1',
+    'author' => 'Arttu Authori',
+    'title' => 'Article Title',
+    'journal' => 'The Journal',
+    'year' => '1922',
+    'volume' => '3b',
+    'number' => '8',
+    'pages' => '24--9283',
+    'month' => 'June',
+    'note' => 'Some result it is!',
+  } }
+
+  it 'requires an entry type' do
+    article_entry['entry_type'] = nil
+    expect(EntryValidator.validate article_entry).to eq false
+  end
+
+  it 'requires a cite key' do
+    article_entry['cite_key'] = nil
+    expect(EntryValidator.validate article_entry).to eq false
+  end
+
+  describe 'article' do
+    it 'validates a correct article class' do
+      expect(EntryValidator.validate article_entry).to eq true
+    end
+
+    it 'requires author' do
+      article_entry['author'] = nil
+      expect(EntryValidator.validate article_entry).to eq false
+    end
+
+  end
 end
 
