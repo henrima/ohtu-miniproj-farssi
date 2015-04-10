9e44738b8ae60f8abd34df616324757198f99055
JjyKs <jyri.saukkonen@gmail.com>
2015-04-10 11:55:37 +0300

    bibtext Exporter

--- lib/bibtex_exporter.rb
+++ lib/bibtex_exporter.rb
@@ -1,3 +1,43 @@
 class BibtexExporter
+  def self.generate(type, key, inputHash)
+    puts "@" + type + "{" + key + ","
 
+    index = inputHash.length
+    ending = ","
+
+    inputHash.each do |key, value|
+      index -= 1
+      ending = "" if index == 0
+      puts "  " + key + " = " + fixScandicCharacters(value) + ending
+    end
+
+    puts "}"
+  end
+
+  def self.fixScandicCharacters(string)
+    fixedString = ""
+    fixes = { "ä" => '{\"a}'}
+
+      string.split("").each do |i|
+
+        if (fixes.has_key?(i))
+          fixedString += fixes[i]
+        else
+          fixedString += i
+        end
+      end
+
+      return fixedString
+    end
+
+
+
+
+  input =   { "author" => "Kake", "title" => "Nykänen vankilassa", "journal" => "Seitsemän päivää",
+              "year" => "2014", "volume" => "asdasd", "number" => "34", "pages" => "13-14", "month" => "feb",
+              "crossref" => "KB1", "note" => "hienoa"}
+
+  generate("article", "KB1", input)
 end
+
+
