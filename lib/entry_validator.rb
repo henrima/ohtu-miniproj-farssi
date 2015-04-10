dcbccc0ba5c58c0c485b5543a1b468216e9a079c
Kristian Setälä <kristian.setala@gmail.com>
2015-04-10 11:47:43 +0300

    basic validation

--- lib/entry_validator.rb
+++ lib/entry_validator.rb
@@ -1,4 +1,9 @@
 module EntryValidator
-
+  def EntryValidator.validate entry
+    return false if !entry['entry_type']
+    return false if !entry['cite_key']
+    return false if !entry['author']
+    true
+  end
 end
 
