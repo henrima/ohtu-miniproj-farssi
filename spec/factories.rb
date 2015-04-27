0e5377dc3d112b2400cee9dd6a6ac795cb30b9b3
Jonne H <jonne.huotari@cs.helsinki.fi>
2015-04-27 12:40:21 +0300

    fixed felix tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,13 +2,8 @@ FactoryGirl.define do
 
   factory :entry do
     category "ARTICLE"
-    field1 = Field.new
-    field1.name = "title"
-    field1.content = "otsikko"
-
-    field2 = Field.new
-    field2.name = "author"
-    field2.content = "jussi"
+    
+    #fields = [field2, field1]
   end
 
   factory :field do
