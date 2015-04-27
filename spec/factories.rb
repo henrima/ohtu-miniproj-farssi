003172e05e100a58836f814d3f916defe5f8b87d
Kristian Setälä <kristian.setala@gmail.com>
2015-04-27 12:38:07 +0300

    näinki toimii

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,15 +2,6 @@ FactoryGirl.define do
 
   factory :entry do
     category "ARTICLE"
-    field1 = Field.new
-    field1.name = "title"
-    field1.content = "otsikko"
-
-    field2 = Field.new
-    field2.name = "author"
-    field2.content = "jussi"
-
-    fields = [field2, field1]
   end
 
   factory :field do
