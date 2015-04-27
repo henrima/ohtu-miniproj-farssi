f639d28e11cbf37eed699bfa8d9205fae52dc27c
JjyKs <jyri.saukkonen@gmail.com>
2015-04-27 11:39:13 +0300

    Tests

--- spec/lib/bibtex_exporter_spec.rb
+++ spec/lib/bibtex_exporter_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe BibtexExporter do
 
@@ -8,4 +8,12 @@ RSpec.describe BibtexExporter do
     end
   end
 
+  describe "bibtex" do
+    it "gets generated from hash" do
+      input =   { "author" => "KÄa3ke", "title" => "Nykänen vankilassa", "journal" => "Seitsemän päivää",
+                  "year" => "2014", "volume" => "asdasd", "number" => "34", "pages" => "13-14", "month" => "feb",
+                  "crossref" => "KB1", "note" => "hienoa"}
+      expect(BibtexExporter.generate("article", "KB1", input)).to eq("@article{KB1,\n  author = K{\\\"A}a3ke,\n  title = Nyk{\\\"a}nen vankilassa,\n  journal = Seitsem{\\\"a}n p{\\\"a}iv{\\\"a}{\\\"a},\n  year = 2014,\n  volume = asdasd,\n  number = 34,\n  pages = 13-14,\n  month = feb,\n  crossref = KB1,\n  note = hienoa\n}")
+    end
+  end
 end
